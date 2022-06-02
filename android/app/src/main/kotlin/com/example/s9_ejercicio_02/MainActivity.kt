2442e7b60c47ca8b4a917f6e33071c1441397032
Yimi-17 <54036463+Yimi-17@users.noreply.github.com>
2022-06-02 15:57:41 -0500

    Subiendo

---
+++ android/app/src/main/kotlin/com/example/s9_ejercicio_02/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.s9_ejercicio_02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('AppBar Flutter'),
+          ),
+          body: Container(
+            height: 400,
+            width: 300,
+            color: Colors.green,
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.topRight,
+            child: Text(
+              'Mi segundo App',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+              ),
+            ),
+          ),
+        ));
+  }
+}
